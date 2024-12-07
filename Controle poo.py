9cef59b0cf60f7d780be41281979bba3784756fe
meriemmoukrim <meriemmoukrim76@gmail.com>
2024-12-07 19:26:12 +0100

    Update Controle poo.py

--- Controle poo.py
+++ Controle poo.py
@@ -1,91 +1,100 @@
-from datetime import *
-class Titulaire :
-    def __init__(self,nom,prenom):
+from datetime import datetime
+
+class Titulaire:
+    def __init__(self, nom, prenom):
         self.__nom = nom
         self.__prenom = prenom
 
     @property
     def nom(self):
-        return   self.__nom
+        return self.__nom
+
     @nom.setter
-    def nom(self,nouvelleNom):
+    def nom(self, nouvelleNom):
         self.__nom = nouvelleNom
 
     @property
     def prenom(self):
-        return self.__nom
+        return self.__prenom
+
     @prenom.setter
     def prenom(self, nouvellePrenom):
-        self.__nom = nouvellePrenom
+        self.__prenom = nouvellePrenom
 
     def __str__(self):
-        return (f"le nom :{self.__nom}\n"
-                f"le prenom : {self.__prenom}")
+        return f"le nom : {self.__nom}\nle prenom : {self.__prenom}"
 
-class CompteBancaire :
+class CompteBancaire:
     __auto = 0
-    def __init__(self , titulaire , dateCeaction , sold = 0 , decouvertAutoriser = 0 ):
+
+    def __init__(self, titulaire, dateCreation, solde=0, decouvertAutorise=0):
         CompteBancaire.__auto += 1
-        self.__numero =  CompteBancaire.__auto
+        self.__numero = CompteBancaire.__auto
         self.__titulaire = titulaire
-        self.__datecreation =  datetime.strptime(dateCeaction, "%d/%m/%Y")
-        self.__solde = sold
-        self.__decouvertAutoriser = decouvertAutoriser
+        self.__dateCreation = datetime.strptime(dateCreation, "%d/%m/%Y")
+        self.__solde = solde
+        self.__decouvertAutorise = decouvertAutorise
 
     @property
     def titulaire(self):
-        return self.__numero
+        return self.__titulaire
+
     @titulaire.setter
     def titulaire(self, nouveautitule):
         self.__titulaire = nouveautitule
 
     @property
-    def datecreation(self):
-        return  self.__datecreation
-    @datecreation.setter
-    def datecreation(self, nouvelleDate):
-        self.__datecreation = nouvelleDate
+    def dateCreation(self):
+        return self.__dateCreation
+
+    @dateCreation.setter
+    def dateCreation(self, nouvelleDate):
+        self.__dateCreation = nouvelleDate
 
     @property
     def solde(self):
         return self.__solde
+
     @solde.setter
     def solde(self, nouveauSolde):
         self.__solde = nouveauSolde
 
-    def depot(self,montant):
+    def depot(self, montant):
         if montant > 0:
             self.__solde += montant
-            return (f"Le montant depot est [{montant}DH ], solde est [{self.__solde} DH]")
-        return (f"le montant est n'est pas en depot")
+            return f"Le montant déposé est [{montant} DH], solde est [{self.__solde} DH]"
+        return "Le montant n'est pas valide pour un dépôt"
 
-    def retrait(self,montant):
-        if montant > 0 :
-            if self.__solde >= montant :
-               self.__solde -= montant
-            return (f"le montant retrait est [{montant}DH] , solde est [{self.solde} DH]")
-        return (f"le montant n'est pas e retrait")
+    def retrait(self, montant):
+        if montant > 0:
+            if self.__solde >= montant:
+                self.__solde -= montant
+                return f"Le montant retiré est [{montant} DH], solde est [{self.__solde} DH]"
+            return f"Solde insuffisant pour un retrait de [{montant} DH]"
+        return "Le montant n'est pas valide pour un retrait"
 
-    def decouverAutoriser(self,montant):
-        if montant > 0 :
-            self.__solde += montant
-            return self.__solde
-        return (f"le decouvert autorisé ne peut pas etre negatif ")
+    def decouvertAutorise(self, montant):
+        if montant >= 0:
+            self.__decouvertAutorise = montant
+            return f"Le découvert autorisé est de [{self.__decouvertAutorise} DH]"
+        return "Le découvert autorisé ne peut pas être négatif"
 
     def __str__(self):
-        return(f"Titulaire : { self.__titulaire }\n"
-               f"la date de creation : {self.__datecreation}\n"
-               f" le solde initiale : { self.__solde}DH\n"
-               f"le depot : {self.depot(400)}\n"
-               f"retrait : {self.retrait(100)}\n"
-               f"le decouvert : {self.decouverAutoriser} DH")
+        return (f"° Titulaire : {self.__titulaire}\n"
+                f"° Date de création : {self.__dateCreation}\n"
+                f"° Solde initial : {self.__solde} DH\n"
+                f"° {self.depot(400)}\n"
+                f"° {self.retrait(100)}\n"
+                f"° {self.decouvertAutorise(500)}\n")
 
-T1 = Titulaire("meriem","moukrim")
+# Test
+T1 = Titulaire("Meriem", "Moukrim")
 print(T1)
-C1 = CompteBancaire("meriem moukrim ","08/05/2025",8000)
+C1 = CompteBancaire(T1, "08/05/2025", 8000)
 print(C1)
 
 
+  
 
 
 
